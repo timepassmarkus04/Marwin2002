ab0c1bd061a889c7d10c3004a8786815b8efb1d2
timepassmarkus04 <markusmarwin2002@gmail.com>
2024-08-07 01:47:49 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -10,7 +10,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
@@ -27,11 +27,11 @@ def private_panel(_):
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/daily_grieff"),
+            InlineKeyboardButton(text=_["S_B_7"], url=f"https://youtu.be/kglflJ-3lpI?si=sPKSwGFqZt5MFAt5"),
         ],
     ]
     return buttons
