fd448bbc5473bf7509b4f3f6ee1400c5525c3780
timepassmarkus04 <markusmarwin2002@gmail.com>
2024-08-07 16:50:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ load_dotenv()
 # Get this value from my.telegram.org/apps
 API_ID = 29350132
 API_HASH = "e854995be05edb5bf21f5b84bdc0212f"
-BOT_TOKEN = "7324085779:AAF5EKjh_rJLuyzfKXbla2jScoq05nQQZj4"
+BOT_TOKEN = "7324085779:AAE_b-iI2nnpAmSFTBq2JHERBlQOMWlOAKU"
 MONGO_DB_URI = "mongodb+srv://devilkingp0404:devilkingp0404@cluster0.iw04afs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 16000))
 LOG_GROUP_ID = -1002222142844
